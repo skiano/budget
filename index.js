6c7df885e9175ec813f95a6502a9dec9ba762d82
Greg Skiano <gregoryskiano@gmail.com>
2015-03-28 11:03:48 -0400

    changing data schema

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 
 var formatBudget = require('./lib/formatBudget');
 
-output = formatBudget({});
+var accounts = [
+  {
+    name: 'giving',
+    cost: 0.1
+  },
+  {
+    name: 'saving',
+    cost: 0.1
+  },
+  {
+    name: 'fishing',
+    cost: 100
+  },
+  {
+    name: 'sailing',
+    cost: 200
+  }
+];
 
+output = formatBudget({
+  taxes: 0.3,
+  accounts: accounts
+});
 
 console.log(output);
